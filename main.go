6d1fdcd79a3a890a92062a92a338ca3cf1a0fcc6
Hans Weisheimer <hweisheimer@users.noreply.github.com>
2024-05-10 00:36:48 -0400

    Normalization mostly complete

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"slices"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -29,9 +33,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Received word list contents:\n", wordList)
+	wordList = normalize(wordList)
+	// TODO: the things
 }
 
 func normalize(words string) string {
-	return "lol"
+	words = strings.ToLower(words)
+
+	// multi-line mode with ^$ mysteriously doesn't work well (only matches first line)
+	// whitespace (\s) won't match file boundaries
+	matcher := regexp.MustCompile(`\b.+'s\b`)
+
+	wordsSlice := matcher.Split(words, -1)
+	words = strings.Join(wordsSlice, "")
+	cleanWordRunes := slices.DeleteFunc([]rune(words), func(r rune) bool {
+		// would be more comprehensive to remove anything that's not alpha, but then I'd need another test ;)
+		return unicode.In(r, unicode.Punct, unicode.White_Space)
+	})
+
+	return string(cleanWordRunes)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -11,13 +12,41 @@ func expect(expected, actual string, t *testing.T) {
 }
 
 func TestNormalize_RemovesPunctuation(t *testing.T) {
-	expected := "passiveaggressive"
-	actual := normalize("passive-aggressive")
+	expected := "eatsshootsleaves"
+	actual := normalize("eats,shoots&leaves-")
 	expect(expected, actual, t)
 }
 
 func TestNormalize_LowersCase(t *testing.T) {
 	expected := "lowercase"
 	actual := normalize("LowerCase")
+	expect(expected, actual, t)
+}
+
+func TestNormalize_RemovesSimpleAccents(t *testing.T) {
+	expected := "e"
+	actual := normalize("\u00e9")
+	expect(expected, actual, t)
+}
+
+func TestNormalize_RemovesCursedAccents(t *testing.T) {
+	expected := "he comes"
+	actual := normalize("h̺̼̞̼͇̮̖̭̗̳̳̣̜̦̬̟̻̄͐͗̎͂ͤ̄̌͆͂ͩ͑̿͛̏͂̇̚e͓͖̰̹̯̬͙̼͇̊ͯͫ̈̊ͩ̔ͣͤ̾͂ ̮̭̙̂ͪ̏̿ͫ̇̐̆͗̐͂ͮͣ̂c͔̪̣͊͋͑̆ͪͯ̍ͩ̎͌͛͋̆͑͗ͅo͍̭̟͎͓̹̖͔̱̼͉̪̪͕͖̭͐̇ͤͯ͛͂͛̅̔̓̋͒̊̐ͩm̯̭͖͚͇̯̠̫͔̼͔̟̯̪̲͛͐̈̃̀̈́́ͨ̽̔̏ͪ̅͐͐͗̂ͮ̔ê͎͚͎͇̣̟̺͇̲͉̱̫ͬ̒̐̉ͥ̐ͭͭͫ̔͐̈́ͨ͑s͉̫̥̬̠̤̭̙̿̑̃̾͒̌ͧ͛̍̚")
+	expect(expected, actual, t)
+}
+
+func TestNormalize_RemovesPossessives(t *testing.T) {
+	actual := normalize("parson's\nperson\nperson's\r\npersonal")
+	if strings.Contains(actual, "parson's") {
+		t.Error("Result should not contain \"parson's\"")
+	}
+	if strings.Contains(actual, "person's") {
+		t.Error("Result should not contain \"person's\"")
+	}
+}
+
+func TestNormalize_RemovesWhitespace(t *testing.T) {
+	expected := "correcthorsebatterystaple"
+	actual := normalize(" correct\rhorse\n battery\r\n\tstaple")
 	expect(expected, actual, t)
 }
